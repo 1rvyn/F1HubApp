f66efd4724630fb12b8b8128970b8d10e89510e8
1rvyn <i.hall3@rgu.ac.uk>
2021-10-22 15:34:02 +0100

    final commit

--- app/src/main/java/com/example/f1hub/ConstructorData.java
+++ app/src/main/java/com/example/f1hub/ConstructorData.java
@@ -1,10 +1,8 @@
 package com.example.f1hub;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,10 +10,11 @@ import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ConstructorsLayoutFragment#newInstance} factory method to
+ * Use the {@link ConstructorData#newInstance} factory method to
  * create an instance of this fragment.
+ *
  */
-public class ConstructorsLayoutFragment extends Fragment implements View.OnClickListener {
+public class ConstructorData extends Fragment implements View.OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,26 +25,26 @@ public class ConstructorsLayoutFragment extends Fragment implements View.OnClick
     private String mParam1;
     private String mParam2;
 
-    public ConstructorsLayoutFragment() {
-        // Required empty public constructor
-    }
-
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment ConstructorsLayoutFragment.
+     * @return A new instance of fragment ConstructorData.
      */
     // TODO: Rename and change types and number of parameters
-    public static ConstructorsLayoutFragment newInstance(String param1, String param2) {
-        ConstructorsLayoutFragment fragment = new ConstructorsLayoutFragment();
+    public static ConstructorData newInstance(String param1, String param2) {
+        ConstructorData fragment = new ConstructorData();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
+    }
+
+    public ConstructorData() {
+        // Required empty public constructor
     }
 
     @Override
@@ -61,18 +60,16 @@ public class ConstructorsLayoutFragment extends Fragment implements View.OnClick
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_constructors_layout, container, false);
-
-        Button btnGetConstructorData = view.findViewById(R.id.btnGetConstructorData);
-        btnGetConstructorData.setOnClickListener(this);
-
+        View view = inflater.inflate(R.layout.fragment_constructor_data, container, false);
+        
+        Button btnHomePage2 = view.findViewById(R.id.btnHomePage2);
+        btnHomePage2.setOnClickListener(this);
+        
         return view;
     }
-
-    public void onClick(View v) {
-        if (v.getId() == R.id.btnGetConstructorData){
-            Navigation.findNavController(v).navigate(R.id.action_constructorsLayoutFragment_to_constructorData2);
+    public void onClick(View v){
+        if (v.getId() == R.id.btnHomePage2) {
+            Navigation.findNavController(v).navigate(R.id.action_constructorData2_to_locationSelectionFragment);
         }
     }
 }
-
